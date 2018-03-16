b652ff2bad23a3d12da9c5d68704ee69a935d8d4
Mansoor A <mansoor.a@livestream.com>
2018-03-16 21:13:59 +0530

    Version3

--- demo_app/server.js
+++ demo_app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world - v2\n');
+  res.send('Hello world - v3\n');
 });
 
 app.listen(PORT, HOST);
